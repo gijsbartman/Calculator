6eba1f53e19760bc6f16967d39e266f5342be5e6
Gijs Bartman <gijs.bartman@gmail.com>
2022-08-14 14:55:55 +0200

    Add button functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-const grid = document.getElementById('grid');
+const numberButtons = document.querySelectorAll("[data-number]")
+const operatorButtons = document.querySelectorAll("[data-operator]")
+
+const clearButton = document.getElementById("clearBtn")
+const input = document.getElementById("input")
+
+clearButton.addEventListener('click', () => clear())
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => appendNumber(button.textContent))
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => appendOperator(button.textContent))
+});
+
+
+
+function appendNumber(number) {
+    if (input.textContent !== "0") {
+        input.textContent = input.textContent + number
+        return
+    }
+    input.textContent = number
+}
+
+function appendOperator(operator) {
+    console.log("HI")
+    if (input.textContent !== "0") {
+        input.textContent = input.textContent + operator
+        return
+    }
+    return
+}
+
+function clear() {
+    input.textContent = "0"
+}
